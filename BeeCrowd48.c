9e7702e2fa1492ac1f47382bead7575a4fcc61b5
Gustasssdev <gustavosousacarvalho875@gmail.com>
2025-04-03 16:59:42 -0300

    Commits BeeCrowd

--- BeeCrowd48.c
+++ BeeCrowd48.c
@@ -4,44 +4,46 @@ int main(){
 
 
     float salario, n_salario; 
-
+    char i = '%';
     scanf("%f", &salario);
 
     if (0 < salario && salario <= 400)
     {
         n_salario = salario + (salario * 15) / 100;
-        printf("Novo Salario: %.2f\n", n_salario);
+        printf("Novo salario: %.2f\n", n_salario);
         printf("Reajuste ganho: %.2f\n", n_salario - salario);
-        printf("Em percentual: 15 %%\n");  
+        printf("Em percentual: 15 %c\n", i);  
     }
 else if (400.01 < salario && salario <= 800)
 {
         n_salario = salario + (salario * 12) / 100;
-        printf("Novo Salario: %.2f\n", n_salario);
+        printf("Novo salario: %.2f\n", n_salario);
         printf("Reajuste ganho: %.2f\n", n_salario - salario);
-        printf("Em percentual: 12 %%\n"); 
+        printf("Em percentual: 12 %c\n", i); 
 }
 
 else if (800.01 < salario && salario<= 1200)
 {
     n_salario = salario + (salario * 10) / 100;
-    printf("Novo Salario: %.2f\n", n_salario);
+    printf("Novo salario: %.2f\n", n_salario);
     printf("Reajuste ganho: %.2f\n", n_salario - salario);
-    printf("Em percentual: 10 %%\n"); 
+    printf("Em percentual: 10 %c\n", i); 
 }    
 
 else if (1200.01 < salario && salario <= 2000)
 {
     n_salario = salario + (salario * 7) / 100;
-    printf("Novo Salario: %.2f\n", n_salario);
+    printf("Novo salario: %.2f\n", n_salario);
     printf("Reajuste ganho: %.2f\n", n_salario - salario);
-    printf("Em percentual: 7 %%\n"); 
+    printf("Em percentual: 7 %c\n", i); 
 }
-else{
+else if (salario >= 2000)
+{
     n_salario = salario + (salario * 4) / 100;
-    printf("Novo Salario: %.2f\n", n_salario);
+    printf("Novo salario: %.2f\n", n_salario);
     printf("Reajuste ganho: %.2f\n", n_salario - salario);
-    printf("Em percentual: 4 %%\n"); 
-
+    printf("Em percentual: 4 %c\n", i); 
 }
+
+
 }
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # BeeCrowd
+Esse repositório é um desempenho gradual da minha evolução no BeeCrowd bem como futuramente servirá para alguém que precise poder revisar um material documentado e bem feito. 
